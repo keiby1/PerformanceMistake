4022b994eb88ab3755c6204d9ddcc90fd508d3f9
Alex <lushinaleksei@bk.ru>
2021-08-10 00:50:23 +0400

    Доработал модуль с неоптимальной джобой

--- src/JobTrouble/JobThread.java
+++ src/JobTrouble/JobThread.java
@@ -5,6 +5,17 @@ import java.util.TimerTask;
 public class JobThread extends TimerTask {
     @Override
     public void run() {
-        System.out.println("runned!");
+
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                for (int k = 0; k < 100; k++) {
+                    for (int l = 0; l < 100; l++) {
+                        Long lon = (long) (i * j * k * l);
+                        System.out.println("i = " + i + " j = " + j + " k = " + k + " l = " + l);
+                    }
+                }
+            }
+        }
+        System.out.println("end!");
     }
 }
--- src/JobTrouble/JobTrouble.java
+++ src/JobTrouble/JobTrouble.java
@@ -8,57 +8,30 @@ import java.util.*;
 
 public class JobTrouble {
     public static void main(String[] args) {
-//        System.out.println("Enter anyone for start");
-//        new Scanner(System.in).next();
-//        System.out.println("started");
-//
-//        Calendar c = Calendar.getInstance();
-//        Calendar c2 = Calendar.getInstance();
-//        c.set(Calendar.HOUR_OF_DAY, 0);
-//        c.set(Calendar.MINUTE, 0);
-//        c2.set(Calendar.HOUR_OF_DAY, 0);
-//        c2.set(Calendar.MINUTE, 37);
-//        Date d = c.getTime();
-//        Date d2 = c2.getTime();
-//
-//
-//        while (true) {
-//
-//            if (System.currentTimeMillis() > d.getTime() && System.currentTimeMillis() < d2.getTime()) {
-//                System.out.println("now");
-//            }
-//
-//            Util.sleep(1000);
-//        }
+        int hour = 0, minutes = 43;
+        int hour2 = 0, minutes2 = 44;
         TimerTask fetchMail = new JobThread();
         Timer timer = new Timer();
-        timer.scheduleAtFixedRate(fetchMail, getTomorrowMorning4am(), fONCE_PER_DAY);
-
-
+        timer.scheduleAtFixedRate(fetchMail, getTomorrowMorning4am(hour, minutes), fONCE_PER_DAY);
+        TimerTask fetchMail2 = new JobThread();
+        Timer timer2 = new Timer();
+        timer2.scheduleAtFixedRate(fetchMail2, getTomorrowMorning4am(hour2, minutes2), fONCE_PER_DAY);
     }
 
 
-
-    private final static long fONCE_PER_DAY = 1000*60*60*24;
-
+    private final static long fONCE_PER_DAY = 1000 * 60 * 60 * 24;
     private final static int fONE_DAY = 0; //today
-    private final static int fFOUR_AM = 1;
-    private final static int fZERO_MINUTES = 9;
 
-    private static Date getTomorrowMorning4am(){
+    private static Date getTomorrowMorning4am(int hour, int minutes) {
         Calendar tomorrow = new GregorianCalendar();
         tomorrow.add(Calendar.DATE, fONE_DAY);
         Calendar result = new GregorianCalendar(
                 tomorrow.get(Calendar.YEAR),
                 tomorrow.get(Calendar.MONTH),
                 tomorrow.get(Calendar.DATE),
-                fFOUR_AM,
-                fZERO_MINUTES
+                hour,
+                minutes
         );
         return result.getTime();
-
     }
-
-
-
 }
