4419a6504d5b7388e75cb7f949b3e0c32ccc6109
Alex <lushinaleksei@bk.ru>
2021-08-12 00:42:01 +0400

    Модуль с "неоптимальным" уровнем логгирования работает одинаково хорошо на любом уровне.

---
+++ src/main/java/LoggerTrouble/LogThread.java
@@ -0,0 +1,29 @@
+package LoggerTrouble;
+
+import org.apache.log4j.Logger;
+
+public class LogThread extends Thread {
+    static final Logger logger = Logger.getLogger(LogThread.class);
+
+    @Override
+    public void run(){
+        while(true){
+
+                logger.trace(
+                        "sd;fgnn".concat("gfdgdfgh").substring(3,6).concat("sadfsdgdsfagfdsg")
+                        .concat("safddsafsgfsdgafdg43qt24t43q")
+                        .concat(Integer.toString(23432*25443543/324234543*56456*435345*54354351/435435354))
+                        .concat("dsfdsfdsgdsgsdg")
+                        .replace("h", "f")
+                        .replace("f", "s")
+                        .replace("h", "f")
+                        .concat("gfdsgsfdgfdsgsfdgsfdg").toString()
+                        .concat(Double.toString(Math.abs(1243)+Math.cos(432423423)*4324324+Math.sin(12342)/14324324+Math.cos(432423423)*4324324+Math.sin(12342)/14324324+Math.cos(432423423)*4324324+Math.sin(12342)/14324324+Math.cos(432423423)*4324324+Math.sin(12342)/14324324))
+                );
+
+                logger.debug("DEBUGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG");
+                logger.info("INFO logggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg");
+                logger.error("Some error");
+        }
+    }
+}
--- src/main/java/LoggerTrouble/LoggerTrouble.java
+++ src/main/java/LoggerTrouble/LoggerTrouble.java
@@ -7,24 +7,14 @@ import java.util.Scanner;
 
 public class LoggerTrouble {
     static final Logger logger = Logger.getLogger(LoggerTrouble.class);
+
     public static void main(String[] args) {
         System.out.println("Enter anyone for start");
         new Scanner(System.in).next();
         System.out.println("started");
 
-        while (true){
-            for (int i = 0; i < 10000; i++) {
-                logger.trace("It's a TRACE");
-            }
-            for (int i = 0; i < 1000; i++) {
-                logger.debug("DEBUGGGG");
-            }
-            for (int i = 0; i < 1000; i++) {
-                logger.info("INFO log");
-            }
-            for (int i = 0; i < 100; i++) {
-                logger.error("Some error");
-            }
+        for (int i = 0; i < 300; i++) {
+            new LogThread().start();
         }
     }
 }
