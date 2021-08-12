8145ae87fab7eaf1378e09f8fa2f4f75a04cc660
Alex <lushinaleksei@bk.ru>
2021-08-12 23:27:14 +0400

    Тест BigString

--- src/main/java/ConcurrentModificationtrouble/ConcurrentModificationException.java
+++
@@ -1,4 +0,0 @@
-package ConcurrentModificationtrouble;
-
-public class ConcurrentModificationException {
-}
--- src/main/java/Experiments/BigString.java
+++ src/main/java/Experiments/BigString.java
@@ -2,9 +2,19 @@ package Experiments;
 
 public class BigString {
     public static void main(String[] args) {
+        long start = System.currentTimeMillis();
         String oneMillionHello = "";
-        for (int i = 0; i < 1000000; i++) {
+        for (int i = 0; i < 120000 ; i++) {
             oneMillionHello = oneMillionHello + "Hello!";
         }
+
+        System.out.println(System.currentTimeMillis()-start);
+
+        StringBuilder sb = new StringBuilder();
+        start = System.currentTimeMillis();
+        for (int i = 0; i < 120000 ; i++) {
+            sb.append("Hello!");
+        }
+        System.out.println(System.currentTimeMillis()-start);
     }
 }
